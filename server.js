ab304467bed732508b373c20298dada9b83235a1
David Maul <dmaul12@gmail.com>
2016-07-14 14:49:28 -0400

    created db seeds schema

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 'use strict'
+const env         = process.env.NODE_ENV || 'development';
+const DEV         = env==='development';
+const dotenv      = (DEV) ? require('dotenv').config() : undefined;
+
 const express       = require('express');
 const logger        = require('morgan');
 const path          = require('path');
